fa0c2002816a64c3f00372f1e0f069bb03ff4458
HeYuan <49941289+Hy-Oy@users.noreply.github.com>
2019-07-20 15:19:07 +0800

    Update models.py

--- user/models.py
+++ user/models.py
@@ -35,7 +35,7 @@ class User(models.Model):
 
     phonenum = models.CharField(max_length=11, unique=True)
     nickname = models.CharField(max_length=16)
-    sex = models.IntegerField(choices=SEXS, default="0")
+    sex = models.IntegerField(choices=SEXS, default=0)
     birth_year = models.IntegerField(default=2000)
     birth_month = models.IntegerField(default=1)
     birth_day = models.IntegerField(default=1)
